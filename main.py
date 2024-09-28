565af4fe16a441fa19081aac0c90845a8c1bd402
Anastasiia-Bilous <ab5731141@gmail.com>
2024-09-28 21:05:44 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-# 1st program
-print(5**0.5*9)
-# 2st program
-print(9.99>9.98 and 1000!=1000.1)
-# 3rd program
-print(2+2*2)
-print((2+2)*2)
-print(2+2*2==(2+2)*2)
-# 4th program
-print(int(float('123.456')*10)%10)
+name = 'Python'# This is a sample Python script.
+print(name)
+name = 'vsego zadah'# Press Shift+F10 to execute it or replace it with your code.
+name = 12# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print(name and name)
+name = 'zatraheno time'
+name = 1.5
+print(name and name)   # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
 
-
-
-
-
-
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
